f8d1f7723acb9f4588bb78ff14c6a10a05a0f011
Siddharth <siddhugosavi89@gmail.com>
2023-12-06 16:52:08 +0530

    increase button counter and change button color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [color, setColor] = useState("#139675");
+
+  function generateColor() {
+    setColor("#" + Math.random().toString(16).substring(12));
+  }
+
+  function action() {
+    setCounter(counter + 1);
+    generateColor();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="btn"
+        style={{ backgroundColor: color }}
+        onClick={() => {
+          action();
+        }}
+      >
+        {counter}
+      </button>
     </div>
   );
 }
